582dacf9186c383b55c816fdca7550decd7924e1
Iker <irodrigueziglesias@danielcastelao.org>
2022-09-26 09:46:15 +0200

    Configurado commits correctamente y proyecto casi acabado

--- app/src/main/java/com/iker/constraint_layout/MainActivity.kt
+++ app/src/main/java/com/iker/constraint_layout/MainActivity.kt
@@ -2,10 +2,21 @@ package com.iker.constraint_layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        constraintLayout = findViewById(R.id.constraint_layout)
     }
+
+    override fun animateToKeyframeTwo() {
+        val constraintSet = ConstraintSet()
+        constraintSet.load(this, R.layout.layout)
+        TransitionManager.beginDelayedTransition()
+        constraintSet.applyTo(constraintLayout)
+    }
+
 }
